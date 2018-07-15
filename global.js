0ccaec0bfa879b3b9da746348c1d197c6f0c946f
BryanMoslo <balgutria@wawand.co>
2018-07-14 20:38:37 -0500

    meme generator

---
+++ global.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#meme").memeGenerator({
+        useBootstrap: true,
+
+        defaultTextStyle: {
+            color: "#FFFFFF",
+            size: 100,
+            lineHeight: 1.2,
+            font: "Impact, Arial",
+            style: "normal",
+            forceUppercase: false,
+            borderColor: "#000000",
+            borderWidth: 5
+        },
+        maxFontSize: 200
+    });
+
+    $("#download").click(function(e){
+        e.preventDefault();
+        $("#meme").memeGenerator("download", "marymeme.png");
+    });
+});
